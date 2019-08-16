668c7fb64dcecfa1467a3f86cbd20a6e7cbf910b
Marko <goncarmarko@gmail.com>
2019-08-17 02:00:39 +0300

    Update spambot.js

--- spambot.js
+++ spambot.js
@@ -2,67 +2,69 @@ let i = 0; // dont touch you bitch
 
 const TwitchBot = require('twitch-bot');
 const BOTNAME = "33kk";
-const OAUTH_TOKEN = "oauth:7mwsqqh8gx3gciymv577t8uzkdczev";
+const OAUTH_TOKEN = "oauth:";
 const CHANNEL = "zakvielchannel"
 const startCommand = "START BOT"
 const stopCommand = "ЗАТКНИСЬ ТВАРЬ"
 const disableCommand = "ЗАТКНИСЬ НАВСЕГДА"
-const text = ("D: ".repeat(15) + " (ТЕКСТ #" + i + ") ").repeat(4) + " для остановки бота на 5 минут напишыте " + stopCommand + " или дайте мут на сколько нужно, плиз не пермач))"
+let   text = ("D: ".repeat(15) + " (ТЕКСТ ТЕКСТ АТАТАТА #" + i + i + ") ").repeat(4) + " для остановки бота на 5 минут напишыте " + stopCommand
 const delay = 2000
 const autowaittime = 5 * 60 * 1000
 
-function BOT() {
-	Bot = new TwitchBot({
-		username: BOTNAME,
-		oauth: OAUTH_TOKEN,
-		channels: [CHANNEL]
-	})
-	
-	let disablebyanyone = false
-	let disabled = false;
-	let autoid = false;
-	let iid = false;
-	
-	Bot.on('join', (...p) => {
-		console.log("JOIN: ", ...p);
-	});
 
-	Bot.on('error', (...p) => {
-		console.log("ERROR: ", ...p);
-	});
-	
-	function start(auto) {
-		console.log((auto ? "AUTO " : "MANUAL ") + "START");
-		disabled = false;
-		if (iid) clearInterval(iid);
-		i = 0;
-		iid = setInterval(() => {
-			i += 1;
-			Bot.say(text);
-			console.log("Message #" + i);
-		}, delay);
-	}
-	
-	Bot.on('message', (msg) => {
-		if (msg.message === startCommand && msg.username === BOTNAME.toLowerCase()) {
-			start(false);
-		}
-		if (msg.message === stopCommand || msg.message == disableCommand) {
-			if (!disabled) {
-				if (iid) clearInterval(iid);
-				console.log("STOP by " + msg.username + " at msg #" + i);
-				if (msg.message == disableCommand && (msg.username === BOTNAME.toLowerCase() || disablebyanyone)) {
-					disabled = true;
-				}
-				else {
-					if (autoid) clearTimeout(autoid);
-					autoid = setTimeout(() => {
-						start(true);
-					}, autowaittime);
-				}
-			}
-		}
-	});
+///////////////////////////////////////////////////
+
+
+Bot = new TwitchBot({
+	username: BOTNAME,
+	oauth: OAUTH_TOKEN,
+	channels: [CHANNEL]
+})
+
+let disablebyanyone = false
+let disabled = false;
+let autoid = false;
+let iid = false;
+
+Bot.on('join', (...p) => {
+	console.log("JOIN: ", ...p);
+});
+
+Bot.on('error', (...p) => {
+	console.log("ERROR: ", ...p);
+});
+
+function start(auto) {
+	console.log((auto ? "AUTO " : "MANUAL ") + "START");
+	disabled = false;
+	if (iid) clearInterval(iid);
+	i = 0;
+	iid = setInterval(() => {
+		i += 1;
+		Bot.say(text);
+		console.log("Message #" + i);
+	}, delay);
 }
 
-BOT();
+Bot.on('message', (msg) => {
+	if (msg.message === startCommand && msg.username === BOTNAME.toLowerCase()) {
+		start(false);
+	}
+	if (msg.message === stopCommand || msg.message == disableCommand) {
+		if (!disabled) {
+			if (iid) clearInterval(iid);
+			iid = false;
+			console.log("STOP by " + msg.username + " at msg #" + i);
+			if (msg.message == disableCommand && (msg.username === BOTNAME.toLowerCase() || disablebyanyone)) {
+				disabled = true;
+			}
+			else {
+				if (autoid) clearTimeout(autoid);
+				autoid = false;
+				autoid = setTimeout(() => {
+					start(true);
+				}, autowaittime);
+			}
+		}
+	}
+});
